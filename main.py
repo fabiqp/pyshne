a0f571dfcc5575f6494ba291cdd5470f3f908f5d
fabiqp <fabiqgm@gmail.com>
2024-11-29 13:11:34 +0100

    added cd to parent dir

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ user = os.popen("echo $USER").read().strip()
 hostname = os.popen("cat /etc/hostname").read().strip()
 
 while True:
-    command = input(f"{user} on {hostname} λ ")
+    path = os.getcwd()
+    command = input(f"{user} on {hostname} λ {path} ")
     if command == "exit":
         break
+    if command.startswith("cd"):
+        os.chdir(f"{path}/{command[3:]}")
     os.system(command)
 
