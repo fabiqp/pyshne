9028116f9e90f98e7845f1e80fdbda392c983aa5
fabiqp <fabiqgm@gmail.com>
2024-11-30 00:21:02 +0100

    added dir copleater and app compleater

--- main.py
+++ main.py
@@ -1,4 +1,39 @@
 import os
+import readline
+
+#dir suggestion function
+def file_completer(text, state):
+    path = os.getcwd()
+    suggestions = [
+        name + ("/" if os.path.isdir(os.path.join(path, name)) else "")
+        for name in os.listdir(path)
+        if name.startswith(text)
+    ]
+    return suggestions[state] if state < len(suggestions) else None
+
+# setting readline to execute file_copleter fun on tab
+readline.set_completer(file_completer)
+readline.parse_and_bind("tab: complete")
+
+
+def file_completer(text, state):
+    paths = os.environ["PATH"].split(":")
+    executables = set()
+
+    for path in paths:
+        if os.path.exists(path):
+            executables.update(
+                name for name in os.listdir(path)
+                if os.access(os.path.join(path, name), os.X_OK) and name.startswith(text)
+            )
+
+    suggestions = sorted(executables)
+    return suggestions[state] if state < len(suggestions) else None
+
+readline.set_completer(file_completer)
+readline.parse_and_bind("tab: complete")
+
+
 
 user = os.popen("echo $USER").read().strip()
 hostname = os.popen("cat /etc/hostname").read().strip()
@@ -9,9 +44,14 @@ while True:
     if command == "exit":
         break
     if command.startswith("cd "):
-        if command == "cd":
+        new_path = command[3:].strip()
+        if new_path == "":
             os.chdir(f"/home/{user}/")
-        os.chdir(f"{path}/{command[3:]}")
+        else:
+            os.chdir(os.path.abspath(new_path))
+
+    elif command == "listdir":
+        print(os.listdir())
     else:
         os.system(command)
 
